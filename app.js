9e2b942f32ff16ba5d8bd921104cf61a3861003a
Jaco Raubenheimer <jaco@trueanthem.com>
2017-05-15 22:05:15 -0700

    make it better

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var express = require('express')
 var cookieParser = require('cookie-parser')
+var port = process.env.PORT | 3000
 
 var app = express()
-  app.use(cookieParser())
+app.use(cookieParser())
 
-  app.get('/login', function (req, res) {
-      res.send('Hello cookie!!! [' + req.cookies.testcookie +']');
-  })
-  app.post('/200', function (req, res) {
-      res.cookie("testcookie","hi - 200!", {"httpOnly":true})
-      res.send('<html><<head><script>window.location="/redirect"</script></head>/html>');
-  })  
-  app.post('/302', function (req, res) {
-      res.cookie("testcookie","hi - 302!", {"httpOnly":true});
-      res.redirect(302, "/redirect");
-  })
-  app.get("/redirect", function(req, res) {
-      res.redirect("/login");
-  })
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.get('/login', function (req, res) {
+    res.send('Hello cookie!!! [' + req.cookies.testcookie +']');
+})
+app.post('/200', function (req, res) {
+    res.cookie("testcookie","hi - 200!", {"httpOnly":true})
+    res.send('<html><<head><script>window.location="/redirect"</script></head>/html>');
+})  
+app.post('/302', function (req, res) {
+    res.cookie("testcookie","hi - 302!", {"httpOnly":true});
+    res.redirect(302, "/redirect");
+})
+app.get("/redirect", function(req, res) {
+    res.redirect("/login");
+})
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
 })
